0a9300ecc6384369b1ae050f0f088358c463c9d1
ChristinaRaether <christinarae860@gmail.com>
2024-01-22 18:43:01 -0800

    attempted testing tc_0

--- src/test/suite/parser.test.ts
+++ src/test/suite/parser.test.ts
@@ -1,4 +1,4 @@
-// import * as assert from 'assert'
+// import * as assert from 'assert' -- this one is from node
 import { Parser } from '../../parser';
 import { describe, suite, test, before } from 'mocha';
 import { expect, assert } from 'chai';
@@ -17,24 +17,44 @@ suite('Parser Test Suite', () => {
     describe('It initializes correctly', () => {
         before(() => {
             // declare var and assign it to a test file
-            file = path.join(__dirname, '')
+            file = path.join(__dirname, '../test_cases/tc_0/index.js');
+            // file = path.join(__dirname, '../../../src/test/test_apps/test_0/index.js');
+
             // pass the var into the new instance of Parser
             parser = new Parser(file);
         });
 
         test('It instantiates an object for the parser class', () => {
             assert.typeOf(parser, 'object', 'Value of new instance should be an object');
+            // expect(parser).to.be.an('object');
         });
 
         test('It begins with a suitable entry file and a tree that is not yet defined', () => {
-            assert.strictEqual(parser.entryFile, file, 'These files are strictly equal');
-            //OR 
-            expect(parser.entryFile).to.equal(file);
             // below is my code
+            assert.strictEqual(parser.entryFile, file, 'These files are strictly equal');
             assert.isUndefined(tree, 'Tree is defined');
+            //OR
+            // expect(parser.entryFile).to.equal(file);
+            // expect(parser.entryFile).to.equal(file);
         });
     });
+
+    // TEST ?: UNPARSED TREE TEST FOR REACT 18(createRoot)
+
 	// TEST 0: ONE CHILD
+    // describe('It works for simple apps', () => {
+    //     before(() => {
+    //         file = path.join(__dirname, '');
+    //         parser = new Parser(file);
+    //         tree = parser.parse();
+    //     });
+
+    //     test('It returns an defined object tree when parsed', () => {
+    //         assert.typeOf(tree, 'object', 'Value of parse() on new instance should be an object');
+    //     });
+    // });
+
+    // TEST 0.5: CHECK IF COMPONENT IS CLIENT OR SERVER (USING HOOKS) => RENDERS A CERTAIN COLOR
 	// TEST 1: NESTED CHILDREN
 	// TEST 2: THIRD PARTY, REACT ROUTER, DESTRUCTURED IMPORTS
 	// TEST 3: IDENTIFIES REDUX STORE CONNECTION
